8adc76ac6bd0ea942907db436e4537c225509196
Ayg0 <ted-dafi@student.1337.ma>
2022-08-14 09:46:19 +0100

    tests

--- CPP/ex01/phonecontact.cpp
+++ CPP/ex01/phonecontact.cpp
@@ -6,7 +6,7 @@
 /*   By: ted-dafi <ted-dafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 09:34:59 by ted-dafi          #+#    #+#             */
-/*   Updated: 2022/06/22 12:40:22 by ted-dafi         ###   ########.fr       */
+/*   Updated: 2022/06/24 10:47:40 by ted-dafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,74 @@ class Contact
 		std::string	End;
 };
 
+class VarNames
+{
+	public:
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nickname;
+		std::string	phonenum;
+		std::string	darkest_secret;
+		VarNames()
+		{
+			first_name = "First Name: ";
+			last_name = "Last Name: ";
+			nickname = "NickName: ";
+			phonenum = "Phone Number: ";
+			darkest_secret = "Darkest Secret: ";
+		}
+};
+
 void	print_string(int len, char *s)
 {
-	std::cout << s << '\n';
+	while (len < 9)
+	{
+		std::cout << ' ';
+		len++;
+	}
+	if (len > 9)
+	{
+		s[9] = '.';
+		s[10] = '\0';
+	}
+	std::cout << s;
 }
 
-void	check_lens(Contact contact, int *batata)
+void	display_contact(Contact contact, int index)
 {
 	std::string	*s;
+	int			i;
 
 	s = (std::string *)&contact;
-	while (!s->empty())
+	i = 3;
+	while (i && !s->empty())
 	{
+		if (i == 3)
+			std::cout << "        " << index + 1 << '|';
 		print_string(s->length(), (char *)&(*s));
 		s++;
+		if (--i)
+			std::cout << '|';
+		else
+			std::cout << '\n';
+	}
+}
+
+void	display_all(Contact contact)
+{
+	VarNames	values;
+	std::string	*s;
+	std::string	*s2;
+	int			i;
+
+	s = (std::string *)&contact;
+	s2 = (std::string *)&values;
+	while (!s->empty())
+	{
+		std::cout << *s2;
+		std::cout << *s << '\n';
+		s++;
+		s2++;
 	}
 }
 
@@ -44,11 +98,12 @@ class PhoneBook
 {
 	private:
 		Contact contacts[8];
-		int		batata[4];
+		int 	i;
+		int		j;
 	public:
-		int i;
 		void set_contact(int index)
 		{
+			
 			std::cout << "Fisrt Name: ";
 			std::cin >> contacts[index].first_name;
 			std::cout << "Last Name: ";
@@ -61,21 +116,44 @@ class PhoneBook
 			std::cin >> contacts[index].darkest_secret;
 			contacts[index].End.clear();
 		}
-		void print(int i)
+		void print(void)
 		{
 			i = 0;
 			while (i < 8)
 			{
-			check_lens(contacts[i], batata);
+				display_contact(contacts[i], i);
 				i++;
 			}
+			std::cout << " >> ";
+			std::cin >> j;
+			if (j > i + 1)
+				std::cout << "Index is out of range or the contact doesn\'t exit use ADD to add a new one.\n";
+			else
+				display_all(contacts[j - 1]);
 		}
 };
 
 int	main()
 {
-	PhoneBook book;
+	PhoneBook	book;
+	int			i;
+	std::string	cmd;
 
+	i = 0;
+	while (1)
+	{
+		std::cout << '>';
+		std::cin >> cmd;
+		if (cmd == "EXIT")
+			return (0);
+		else if (cmd == "ADD")
+			book.set_contact(i);
+		else if (cmd == "SEARCH")
+			book.print();
+		i += 1 * (i < 9) + -i * (i >=9);
+	}
+	
 	book.set_contact(1);
-	book.print(1);
+	book.set_contact(2);
+	book.print();
 }
--- test.c
+++ test.c
@@ -1,12 +1,54 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-void	myStartupFun(void) __attribute__((constructor));
-void	myStartupFun(void)
+//void	myStartupFun(void) __attribute__((constructor));
+//void	myStartupFun(void)
+//{
+//    printf("wtf");
+//}
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+void	get_num(struct ListNode* head, long *n)
 {
-    printf("wtf");
+	int	i;
+
+	i = 1;
+	while (head)
+	{
+		n[0] *= 10;
+		n[0] += head->val;
+		n[1] += (head->val * i);
+		head = head->next;
+		i *= 10;
+	}
 }
-  
-int main(void)
+
+bool isPalindrome(struct ListNode* head){
+
+	long	n[2] = {0};
+	get_num(head, n);
+	return (n[1] == n[0]);
+}
+
+int	main()
 {
+	struct ListNode* tmp;
+
+	tmp = malloc(sizeof(struct ListNode));
+	tmp->val = 1;
+	tmp->next = malloc(sizeof(struct ListNode));
+	tmp->next->val = 2;
+	tmp->next->next = malloc(sizeof(struct ListNode));
+	tmp->next->next->val = 2;
+	tmp->next->next->next = malloc(sizeof(struct ListNode));
+	tmp->next->next->next->val = 1;
+	tmp->next->next->next->next = NULL;
+	printf("%d\n", isPalindrome(tmp));
 }
